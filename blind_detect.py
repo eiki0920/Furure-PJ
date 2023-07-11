25237a9fe842ed7681dce3436538fd649fe31f34
eiki0920 <s2122039@stu.musashino-u.ac.jp>
2023-07-11 19:29:36 +0900

    リアルタイムで瞬きを検出

---
+++ blind_detect.py
@@ -0,0 +1,57 @@
+import os,sys
+import cv2
+import dlib
+from imutils import face_utils
+from scipy.spatial import distance
+
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('../haarcascade_frontalface_alt2.xml')
+face_parts_detector = dlib.shape_predictor('../shape_predictor_68_face_landmarks.dat')
+
+# 目のアスペクト比を求める
+def calc_ear(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    beside = distance.euclidean(eye[0], eye[3])
+    vertical = (A + B) / 2
+    eye_ear = vertical / beside
+    return round(eye_ear, 3)
+
+
+while True:
+    # カメラから１フレーム読み込む
+    ret, img = cap.read()
+
+    #画像をグレースケール化
+    img_gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # facesには顔の位置情報が格納されている
+    faces = face_cascade.detectMultiScale(
+            img_gry, scaleFactor=1.11, minNeighbors=3, minSize=(100, 100))
+
+    if len(faces) == 1:
+        x, y, w, h = faces[0, :]
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        face = dlib.rectangle(x, y, x + w, y + h)
+        face_parts = face_parts_detector(img_gry, face)
+        face_parts = face_utils.shape_to_np(face_parts)
+
+        # 右目のアスペクト比を求める
+        right_eye = face_parts[42:48]
+        right_eye_ear = calc_ear(right_eye)
+
+        # 左目のアスペクト比を求める
+        left_eye = face_parts[36:42]
+        left_eye_ear = calc_ear(left_eye)
+
+        if (right_eye_ear + left_eye_ear) < 0.40:
+            cv2.putText(img, "eye close", (10, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow('frame', img)
+
+    if cv2.waitKey(1) == 27:
+        break  # esc to quit
+
+cap.release()
+cv2.destroyAllWindows()
